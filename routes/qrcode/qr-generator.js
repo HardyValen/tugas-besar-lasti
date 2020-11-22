6e59fc7b5fd10e63be07a64638d6b4d396b1c361
HardyValen <hardyvalen@gmail.com>
2020-11-22 21:00:24 +0700

    implemented qr code

--- routes/qrcode/qr-generator.js
+++ routes/qrcode/qr-generator.js
@@ -1,10 +1,35 @@
 const express = require('express');
-const fs = require('fs');
-const stream = require('stream')
+const QRcode = require('qrcode');
+const stream = require('stream');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
-router.get('/', function(req, res) {
-  res.send('OK');
+router.get('/', async function(req, res) {
+  
+  // const r = fs.createReadStream(__dirname + "/buffer.png")
+  // const r = QRcode.toFileStream(stream, "Hello World");
+  // const ps = new stream.PassThrough()
+  // stream.pipeline(r, ps, (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.status(400).send(err)
+  //   }
+  // })
+  // ps.pipe(res);
+
+  try {
+    let content = uuidv4();
+    const qrStream = new stream.PassThrough();
+    await QRcode.toFileStream(qrStream, content, {
+      type: 'png',
+      width: 240,
+      errorCorrectionLevel: 'H'
+    })
+      
+    qrStream.pipe(res);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
 router.get('/page', function(req, res) {
